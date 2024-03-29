ffd9b6419a60ccbdc60b8cf714d2367fff420ca1
Lenny Lemashon <leonardlemashon@gmail.com>
2024-03-29 09:47:03 +0300

    static files

--- SmartCaretaker/settings.py
+++ SmartCaretaker/settings.py
@@ -133,8 +133,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATICFILES_DIRS = [BASE_DIR/'static',]
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Change 'static' to 'staticfiles'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'chatbot', 'static'),
+]
+
 
 
 # Default primary key field type
